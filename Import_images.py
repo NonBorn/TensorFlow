5de32b22f97eeb596239b6fba5bc839509417ddf
nonborn <kapsathas@gmail.com>
2017-07-23 11:24:14 +0300

    Python Lab

--- Import_images.py
+++ Import_images.py
@@ -2,94 +2,138 @@ import os
 import numpy as np
 from PIL import Image
 import regex as re
-
-# define file path of the folder of images
 import tensorflow as tf
 
+# define file path of the folder of images
 path_init = '/Users/nonborn/[MSc] Business Analytics/3rd Semester/[SQ-PT-2017] Social Network Analysis and Social Media Analytics/Assignment (tensorflow)/rendered_256x256/256x256/sketch/tx_000000000000'
-#path_init = '/Users/nonborn/Desktop/test/'
+# path_init = '/Users/nonborn/Desktop/test/'
 
 # print(len(files))
-#print('Folders in Directory of Parent Folder')
-#print(os.listdir(path_init))
+# print('Folders in Directory of Parent Folder')
+# print(os.listdir(path_init))
 
+# Get all list of subfolders into filenames filtering those that start with . (hidden folders)
 reg = re.compile("^\.")
 filenames = [x for x in os.listdir(path_init) if not reg.match(x)]
 
 
-#print('Folders in Directory of Parent Folder - After filtering')
+# print('Folders in Directory of Parent Folder - After filtering')
 #print (filenames)
-#print(len(filenames))
+dep = len(filenames)
 
-#f_im = path_init + filenames[0]
-#print(f_im)
+# f_im = path_init + filenames[0]
+# print(f_im)
 
-#files = [f_im + '/' + f for f in os.listdir(f_im)]
-#print(files)
+# files = [f_im + '/' + f for f in os.listdir(f_im)]
+# print(files)
 
 
 # print('----')
 
-
+# Function to transform an image to an numpy array. Resizing is performed by the function as well using antialiazing technique
 def get_numpy(fpath):
+    height = 64
+    width = 64
+
     im = Image.open(fpath)
-    #im.show();
-    im = im.resize((64, 64), Image.ANTIALIAS)
-    #im.show();
+    # im.show(); # for debugging purposes
+    im = im.resize((height, width), Image.ANTIALIAS) # resize the image to 64 x 64
+    # im.show();
     im = im.convert('L')  # to convert an image to grayscale
     im = np.asarray(im, dtype=np.float32)
     return im
 
+# files_list: list with the names of all the subfolders of the path
+# child_index: pointer which has the starting folder of the files_list
+# position: position index within the folder
 
-def batch_X(files_list, child_index, b_size, position):
+
+
+def batch_X(files_list, child_index, b_size, pos_index):
     lpath = path_init + '/' + files_list[child_index]
     reg1 = re.compile("^\.")
     test = [x for x in os.listdir(lpath) if not reg1.match(x)]
     image_paths = [lpath + '/' + f for f in test]
-    #print(test)
+    # print(test)
 
-    if position + b_size <= len(test):
-        print('true')
-        batch_xx = np.asarray([get_numpy(fpath) for fpath in image_paths[position:position + b_size]])
-        position = position + b_size
+    if pos_index + b_size <= len(test):
+        #print('true')
+        batch_xx = np.asarray([get_numpy(fpath) for fpath in image_paths[pos_index:pos_index + b_size]])
+        position = pos_index + b_size
     else:
-        print('false')
-        image_paths = [x for x in image_paths[position:len(test)]]
-        add_images = b_size - (len(test)-position)
-        #print(add_images)
+        #print('false')
+        image_paths = [x for x in image_paths[pos_index:len(test)]]
+        add_images = b_size - (len(test) - pos_index)
+        # print(add_images)
 
-        position = 0
+        pos_index = 0
         child_index = child_index + 1
         lpath1 = path_init + '/' + files_list[child_index]
         reg1 = re.compile("^\.")
         test = [x for x in os.listdir(lpath1) if not reg1.match(x)]
         image_paths1 = [lpath1 + '/' + f for f in test]
-        image_paths1 = [x for x in image_paths1[position:add_images]]
-        #print (image_paths1)
+        image_paths1 = [x for x in image_paths1[pos_index:add_images]]
+        # print (image_paths1)
 
         image_paths2 = image_paths + image_paths1
-        #print(image_paths2)
+        # print(image_paths2)
 
         batch_xx = np.asarray([get_numpy(fpath) for fpath in image_paths2])
-        position = position + add_images
-    return batch_xx, child_index, position
+        position = pos_index + add_images
+    return batch_xx, child_index, pos_index
+
+
+# def batch_Y(files_list, child_index, b_size, pos_index):
+#     lpath = path_init + '/' + files_list[child_index]
+#     reg1 = re.compile("^\.")
+#     test = [x for x in os.listdir(lpath) if not reg1.match(x)]
+#     image_paths = [lpath + '/' + f for f in test]
+#
+#     if pos_index + b_size <= len(test):
+#
+#     else:
+#
+#     return
+
+
+# Vocuabulary
+text_idx = range(0,125)
+print(text_idx)
+
+vocab_size = len(filenames)
+text_length = len(text_idx)
+one_hot = np.zeros(([vocab_size, text_length]))
+print(len(one_hot))
+one_hot[text_idx, np.arange(text_length)] = 1
+
+one_hot = one_hot.astype(int)
+
+
+print(len(one_hot))
+print(one_hot[124])
+
+
+
+
+
+
+
+
 
 
 child_index = 0
 position = 708
 b_size = 21
 
-
-
 x = batch_X(filenames, child_index, b_size, position)
-#print(x[0],x[2])
-#print(len(x))
-#print(x[0])
-#image = x[0, :]
+# print(x[0],x[2])
+# print(len(x))
+# print(x[0])
+# image = x[0, :]
 print x[0].shape
-print(x[1],x[2])
+print(x[1], x[2])
 
-#np.savetxt(path_init+'/test.txt', x[0], delimiter=';', newline='\n')
+# np.savetxt(path_init+'/test.txt', x[0], delimiter=';', newline='\n')
 
 
 
